d1e05711539bd5dab2e2d308223b3ab4acaa7465
mohammjavad123 <javvadrajabi@gmail.com>
2025-07-09 19:04:30 +0100

    Add pg dependency for PostgreSQL

--- config/database.js
+++ config/database.js
@@ -2,6 +2,8 @@ module.exports = ({ env }) => ({
   connection: {
     client: env('DATABASE_CLIENT', 'postgres'),
     connection: env('DATABASE_URL'),
-    ssl: env.bool('DATABASE_SSL', false),
+    ssl: {
+      rejectUnauthorized: false,  // Accept self-signed or missing certs (common on Render)
+    },
   },
 });
